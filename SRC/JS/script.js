f75b9a9b3ca01a33f811114168b5595e79461705
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-10-27 12:52:46 -0300

    testando valores nos inputs de pesquisa

--- SRC/JS/script.js
+++ SRC/JS/script.js
@@ -1,8 +1,12 @@
 const searchButton = document.getElementById("search-button");
 const modalOverlay = document.querySelector(".modal-overlay");
+const movieName = document.getElementById("movie-name");
+const movieYear = document.getElementById("movie-year");
 
 function searchButtonClickHandler() {
   modalOverlay.classList.add("open");
+  console.log(movieName.value.split(" ").join("+"));
+  console.log("Ano: ", movieYear.value);
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
