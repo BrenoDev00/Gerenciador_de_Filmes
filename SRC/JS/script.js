b9a949122260581c515cddc3fdda7bdb4f5d3356
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-10-28 20:36:26 -0300

    desenvolvimento de funções geradoras de parâmetros de nome e ano do filme e da função assíncrona para consumir api

--- SRC/JS/script.js
+++ SRC/JS/script.js
@@ -2,18 +2,40 @@ const searchButton = document.getElementById("search-button");
 const modalOverlay = document.querySelector(".modal-overlay");
 const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
+const apiKey = "649a4725";
 
 async function searchButtonClickHandler() {
-  modalOverlay.classList.add("open");
+  try {
+    modalOverlay.classList.add("open");
 
-  let url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieName.value
-    .split(" ")
-    .join("+")}`;
+    let url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieNameParameterGenerator()}&y=${movieYearParameterGenerator()}`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
-  console.log(data);
+    console.log(data);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function movieNameParameterGenerator() {
+  if (movieName.value === "") {
+    throw new Error("Informe o nome do filme");
+  }
+  return movieName.value.split(" ").join("+");
+}
+
+function movieYearParameterGenerator() {
+  if (movieYear.value === "") {
+    return "";
+  }
+
+  if (movieYear.value.length !== 4 || isNaN(Number(movieYear.value))) {
+    throw new Error("Ano do filme inválido");
+  }
+
+  return `&y=${movieYear.value}`;
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
